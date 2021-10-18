f8d8875dc3f2ce42b6e685e84ee4849fb83a2000
Yash Deshpande <yashdeshpande8087@gmail.com>
2021-10-18 21:18:08 +0530

    Added main QR generation
    Added main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import datetime
+from os import *
+import time
+
+from MyQR import myqr as mq
+
+
+ROOT_PATH=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+OUTPUT_PATH=os.path.join(ROOT_PATH,'results')
+OUTPUT_NAME='outputQR_'+str(time.strftime('%M%S'))+'.png'
+
+
+def createQRcode(inpURL):
+    print('The given URL is : ', inpURL)
+    mq.run(words=inpURL,save_dir=OUTPUT_PATH,save_name=OUTPUT_NAME)
+
+
+
+if(__name__=='__main__'):
+    if((os.path.exists(OUTPUT_PATH)==False)):
+        os.mkdir(OUTPUT_PATH)
+    print('Welcome to QR Code Generator')
+    cmdURL=input('Please enter the link --> ')
+    createQRcode(cmdURL)
